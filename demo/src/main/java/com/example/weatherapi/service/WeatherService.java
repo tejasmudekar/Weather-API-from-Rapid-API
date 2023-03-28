95819d4945eb476ed57a1fa670ba8eeb79badd2e
tejasmudekar <32029684+tejasmudekar@users.noreply.github.com>
2023-03-28 22:39:30 +0530

    Your commit message here

--- demo/src/main/java/com/example/weatherapi/service/WeatherService.java
+++ demo/src/main/java/com/example/weatherapi/service/WeatherService.java
@@ -8,30 +8,7 @@ import org.springframework.web.client.RestTemplate;
 @Service
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class WeatherService {
-
-
-/*
-    RestTemplate restTemplate;
-    public final Logger logger = LoggerFactory.getLogger(this.getClass().getName());
-
-    public String getWeatherForecast() {
-       // String url="https://tapi.wetter.com/v2.3/rapidapi/forecast/"+cityLoc+"/summary/";
-        String url="http://ip.jsontest.com/";
-        System.out.println(url);
-        HttpHeaders headers = getHttpHeaders();
-        HttpEntity<String> entity=new HttpEntity<String>(headers);
-        ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
-        logger.info("okkkkkkkkkkkkk", response);
-       // ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.GET,entity, String.class);
-        return "Sucess";
-    }
-    private HttpHeaders getHttpHeaders() {
-        HttpHeaders headers = new HttpHeaders();
-        headers.set("Content-Type", "application/json");
-        headers.set("X-Application-ID","com.wetter/ios v1.0");
-        headers.set("Host", "");
-        return headers;
-    }*/
+    
 
     public String RapidApiGetForecastSummaryByLocationName(String location) {
         String url = "https://tapi.wetter.com/v2.3/rapidapi/forecast/"+location+"/summary/";
